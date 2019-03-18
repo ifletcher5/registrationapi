34dceb7352efe2711189d83caa1448795a845999
Ismail <ifletcher5@gmail.com>
2019-03-17 20:35:42 -0400

    Added CORS to registration.go

--- controllers/registration.go
+++ controllers/registration.go
@@ -24,6 +24,7 @@ func NewRegistrationController(db database.Storage) RegistrationController {
 // HandleGetAll handles getting all registrations request
 func (controller RegistrationController) HandleGetAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	log.Println("Handling getting all registrations")
+	enableCors(&w)
 	registrations := controller.db.GetRegistrations()
 	encodedRegistrations, err := json.Marshal(registrations)
 	if err != nil {
@@ -38,6 +39,7 @@ func (controller RegistrationController) HandleGetAll(w http.ResponseWriter, r *
 func (controller RegistrationController) HandleGetOne(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
 	log.Printf("handling getting registration with %s", id)
+	enableCors(&w)
 	if len(id) > 0 {
 		i := controller.db.GetRegistration(id)
 		encodedRegistration, err := json.Marshal(i)
@@ -58,6 +60,7 @@ func (controller RegistrationController) HandleGetOne(w http.ResponseWriter, r *
 func (controller RegistrationController) HandleDeleteRegistration(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
 	log.Printf("handling deleting registration with id %s", id)
+	enableCors(&w)
 	if len(id) > 0 {
 		controller.db.DeleteRegistration(id)
 		w.WriteHeader(http.StatusOK)
@@ -69,6 +72,7 @@ func (controller RegistrationController) HandleDeleteRegistration(w http.Respons
 // HandleNewRegistration handles adding a registration request
 func (controller RegistrationController) HandleNewRegistration(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	log.Println("handling creation of a registration")
+	enableCors(&w)
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -89,4 +93,8 @@ func (controller RegistrationController) HandleNewRegistration(w http.ResponseWr
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write(encodedRegistration)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
